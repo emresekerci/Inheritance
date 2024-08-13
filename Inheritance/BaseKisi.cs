bb4d7995bf8cf81d15cad89447bf7241d2a5efb3
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-13 19:00:22 +0300

    Add project files.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public void KisiBilgisi()
+        {
+            Console.WriteLine($"İsim: {Ad}");
+            Console.WriteLine($"Soyisim: {Soyad}");
+        }
+    }
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrenciNumarasi { get; set; }
+        public void OgrenciBilgi()
+        {
+            Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+            KisiBilgisi();
+        }
+
+
+
+
+    }
+    public class Ogretmen : BaseKisi
+    {
+        public decimal MaasBilgisi { get; set; }
+        public void OgretmenBilgi()
+        {
+            Console.WriteLine($"Maaş: {MaasBilgisi}");
+            KisiBilgisi();
+        }
+
+    }
+
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp10;
+
+Ogrenci ogrenci1 = new Ogrenci();
+ogrenci1.Ad = "Emre";
+ogrenci1.Soyad = "Şekerci";
+ogrenci1.OgrenciNumarasi = 654;
+ogrenci1.OgrenciBilgi();
+Console.WriteLine("------------------------");
+Ogretmen ogretmen1 = new Ogretmen();
+ogretmen1.Ad = "Ahmet";
+ogretmen1.Soyad = "Demir";
+ogretmen1.MaasBilgisi = 46.000m;
+ogretmen1.OgretmenBilgi();
